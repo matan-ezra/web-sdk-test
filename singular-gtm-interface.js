310ad8b375ca4fcb0bcf9043d4f6ce85605ebe53
Matan Ezra <matan@singular.net>
2026-03-02 15:13:27 +0200

    updated singular-gtm-interface.js to trigger data_layer_updated event

--- singular-gtm-interface.js
+++ singular-gtm-interface.js
@@ -314,7 +314,7 @@
 
     function initSingularSdkScript(queue) {
         window.isSingularSdkInitializing = true;
-        loadScript('https://cdn.jsdelivr.net/gh/matan-ezra/web-sdk-test@main/singular-sdk.js', function () {
+        loadScript('https://web-sdk-cdn.singular.net/singular-sdk/latest/singular-sdk.js', function () {
             if (!window.singularSdk) {
                 console.log("Unable to load Singular's Web SDK");
                 window.isSingularSdkInitializing = false;
@@ -340,7 +340,7 @@
         if (!key) return;
         window.dataLayer = window.dataLayer || [];
         window.dataLayer.push({
-            event: 'data_layer_updated',
+            event: `singular.data_layer.${key}.updated`,
             dl_key: key,
             [key]: value
         });
